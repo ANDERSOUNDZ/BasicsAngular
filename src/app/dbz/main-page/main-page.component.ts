5def3078c8d4480c924f253bb3ed97c594ffe5a7
zoundzEc <andersonmikol@live.com>
2023-03-02 16:43:45 -0500

    Add new class

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 
+interface Personaje{
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
+  
+  nuevo: Personaje = {
+    nombre: 'Trunks',
+    poder: 10000
+  }
+
+  // cambiarNombre(event: any){
+  //   console.log(event)
+  // }
+
   agregar(){
-    console.log("Esta es una prueba!");
+    console.log(this.nuevo);
   }
 }
   
